662dd0f533e39db38a1028036a33a41effe7b48b
kfil <koafil@gmail.com>
2022-07-06 15:18:33 +0300

    параметры page и npp

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,31 @@ const router = app => {
     });
   });
   app.get('/tovars',(req,res)=>{
-    pool.query('SELECT * FROM tovars', (error,result) => {
-      if(error) throw error;
-      res.send(result);
+    let numPerPage = parseInt(req.query.npp,10) || 5;
+    let page = parseInt(req.query.page,10) || 0;
+    let skip = page * numPerPage;
+    let numRows;
+    let numPages;
+
+    pool.query('SELECT count(*) as numRows FROM tovars', (error01,result01) => {
+      if(error01) throw error01;
+      numRows = result01[0].numRows;
+      numPages = Math.ceil(numRows/numPerPage);
+      
+      let limit = skip + ',' + numPerPage;
+      console.log(`numRows = ${numRows} numPages = ${numPages}`);
+
+      pool.query('SELECT * FROM tovars LIMIT '+limit, (error02,result02) => {
+        if(error02) throw error02;
+        res.send(result02);
+      });
     });
+    
+    // pool.query('SELECT * FROM tovars', (error,result) => {
+    //   if(error) throw error;
+    //   res.send(result);
+    // });
 //    res.send(req.query);
-//    console.log(req.query);
   });
   app.get('/tovars/:id',(req,res)=>{
     const id = req.params.id;
@@ -23,31 +42,6 @@ const router = app => {
     });
   });
 
-  app.post('/users',(req,res)=>{
-    console.log(req.body);
-    pool.query('INSERT INTO users SET ?', req.body, (error,result)=>{
-      if(error) throw error;
-      res.status(201).send(`User added with ID: ${result.insertId}`);
-    });
-  });
-
-  app.put('/users/:id',(req,res)=>{
-    const id = req.params.id;
-
-    pool.query('UPDATE users SET ? WHERE id = ?',[req.body,id],(error,result)=>{
-      if(error) throw error;
-//      res.send('User updated successfully');
-      res.send(result);
-    });
-  });
-
-  app.delete('/users/:id',(req,res)=>{
-    const id = req.params.id;
-    pool.query('DELETE FROM users WHERE id = ?', id, (error,result)=>{
-      if(error) throw error;
-      res.send('User deleted');
-    });
-  });
 };
 
 module.exports = router;
