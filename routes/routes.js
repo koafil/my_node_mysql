fc2cff07d32ea4f6385712f7390722a561444e76
kfil <koafil@gmail.com>
2023-10-11 17:27:01 +0300

    выборка артикулов, параметр s ключ фильтра

--- routes/routes.js
+++ routes/routes.js
@@ -7,33 +7,61 @@ const router = app => {
       message: 'Node.js and Express REST API 2.0'
     });
   });
+
+  app.get('/articul',(req,res)=>{
+    let numRows;
+    pool.query('SELECT count(articul) as numRows FROM tovars', (error01,result01) => {
+      if (error01) throw error01;
+      numRows = result01[0].numRows;
+      console.log(`\\articul numRows = ${numRows} `);
+    });
+    pool.query('SELECT articul FROM tovars', (error02,result02) => {
+      if (error02) throw error02;
+      let articulList = result02.map(item=>{
+        return item.articul;
+      });
+      res.send({total: numRows, rowData: articulList});
+    });
+  });
+
   app.get('/tovars',(req,res)=>{
+    let findKey = req.query.s || "";
     let numPerPage = parseInt(req.query.npp,10) || 5;
-    let page = parseInt(req.query.page,10) || 0;
-    let skip = page * numPerPage;
+    let page = parseInt(req.query.page,10) || 1;
+    let skip = (page - 1) * numPerPage;
     let numRows;
     let numPages;
+    if (!findKey){
+      pool.query('SELECT count(*) as numRows FROM tovars', (error01,result01) => {
+        if(error01) throw error01;
+        numRows = result01[0].numRows;
+        numPages = Math.ceil(numRows/numPerPage);
 
-    pool.query('SELECT count(*) as numRows FROM tovars', (error01,result01) => {
-      if(error01) throw error01;
-      numRows = result01[0].numRows;
-      numPages = Math.ceil(numRows/numPerPage);
-      
-      let limit = skip + ',' + numPerPage;
-      console.log(`numRows = ${numRows} numPages = ${numPages}`);
+        let limit = skip + ',' + numPerPage;
+        console.log(`numRows = ${numRows} numPages = ${numPages}`);
 
-      pool.query('SELECT * FROM tovars LIMIT '+limit, (error02,result02) => {
-        if(error02) throw error02;
-        res.send(result02);
+        pool.query('SELECT * FROM tovars LIMIT '+limit, (error02,result02) => {
+          if(error02) throw error02;
+          res.send({total: numRows, rowData: result02});
+        });
       });
-    });
-    
-    // pool.query('SELECT * FROM tovars', (error,result) => {
-    //   if(error) throw error;
-    //   res.send(result);
-    // });
-//    res.send(req.query);
+    } else {
+      pool.query(`SELECT count(*) as numRows FROM tovars WHERE articul LIKE '%${findKey}%'`, (error01,result01) => {
+        if(error01) throw error01;
+        numRows = result01[0].numRows;
+        numPages = Math.ceil(numRows/numPerPage);
+
+        let limit = skip + ',' + numPerPage;
+        console.log(`numRows = ${numRows} numPages = ${numPages} findKey = ${findKey}`);
+
+        pool.query(`SELECT * FROM tovars WHERE articul LIKE '%${findKey}%' LIMIT ${limit}`, (error02,result02) => {
+          if(error02) throw error02;
+          res.send({findKey, total: numRows, rowData: result02});
+       });
+      });
+    }
   });
+
   app.get('/tovars/:id',(req,res)=>{
     const id = req.params.id;
     pool.query('SELECT * FROM tovars WHERE id = ?',id, (error,result) => {
