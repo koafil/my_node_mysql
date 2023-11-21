a85d7e2bab288b44a8bde1fe9ebfc02f01498656
kfil <koafil@gmail.com>
2023-11-21 15:14:00 +0300

    ss массив ключей фильтра

--- routes/routes.js
+++ routes/routes.js
@@ -26,12 +26,15 @@ const router = app => {
 
   app.get('/tovars',(req,res)=>{
     let findKey = req.query.s || "";
+    let findKeyArr = req.query.ss || "";
     let numPerPage = parseInt(req.query.npp,10) || 5;
     let page = parseInt(req.query.page,10) || 1;
     let skip = (page - 1) * numPerPage;
     let numRows;
     let numPages;
-    if (!findKey){
+//    console.log(JSON.parse(findKeyArr));
+//    console.log(findKeyArr);
+    if (!findKey && !findKeyArr){
       pool.query('SELECT count(*) as numRows FROM tovars', (error01,result01) => {
         if(error01) throw error01;
         numRows = result01[0].numRows;
@@ -45,7 +48,7 @@ const router = app => {
           res.send({total: numRows, rowData: result02});
         });
       });
-    } else {
+    } else if(findKey){
       pool.query(`SELECT count(*) as numRows FROM tovars WHERE articul LIKE '%${findKey}%'`, (error01,result01) => {
         if(error01) throw error01;
         numRows = result01[0].numRows;
@@ -57,7 +60,34 @@ const router = app => {
         pool.query(`SELECT * FROM tovars WHERE articul LIKE '%${findKey}%' LIMIT ${limit}`, (error02,result02) => {
           if(error02) throw error02;
           res.send({findKey, total: numRows, rowData: result02});
-       });
+      });
+      });
+    } else {
+      let findKeyArrParsed = JSON.parse(findKeyArr);
+      console.log(findKeyArrParsed);
+      console.log(`count = ${findKeyArrParsed.length}`);
+      let strLike= "";
+      if(findKeyArrParsed.length){
+        strLike=`WHERE articul LIKE '%${findKeyArrParsed[0]}%'`;
+        findKeyArrParsed.forEach((v,i,a) => {
+          if(i){
+            strLike=`${strLike} OR articul LIKE '%${v}%'`
+          }
+        });
+      }
+      console.log(strLike);
+      pool.query(`SELECT count(*) as numRows FROM tovars ${strLike}`, (error01,result01) => {
+        if(error01) throw error01;
+        numRows = result01[0].numRows;
+        numPages = Math.ceil(numRows/numPerPage);
+
+        let limit = skip + ',' + numPerPage;
+        console.log(`numRows = ${numRows} numPages = ${numPages} findKey = ${findKey}`);
+
+        pool.query(`SELECT * FROM tovars ${strLike} LIMIT ${limit}`, (error02,result02) => {
+          if(error02) throw error02;
+          res.send({findKey, total: numRows, rowData: result02});
+        });
       });
     }
   });
